7b7f951f241018dc4ea3572134d9f3e4a1ab0155
ANA CLARA RIMUALDO PIRES <24122542@CFP127ALUNO.LOCAL>
2025-02-11 12:04:40 -0300

    Adicionando código ao repositório

---
+++ app.js
@@ -0,0 +1,14 @@
+/************************************************************************************************ 
+ * Objetivo: Criar uma API paa realizar uma integração com o banco de dados
+ * Data:     11.02.2025
+ * Autor:    Ana Pires 
+ * Versão:   1.0
+ * Observações:
+ *** Para criar a API precisa instalar
+ *     express        (npm install express --save)
+ *     cors           (npm install cors --save )
+ *     body-parser    ( npm install body-parser  --save)
+ *** Para criar a conexão com banco de dados precisa instalar:
+ *     prisma         (npm install prisma --save)
+ *     @prisma/client (npm install @prisma/client --save)
+************************************************************************************************/

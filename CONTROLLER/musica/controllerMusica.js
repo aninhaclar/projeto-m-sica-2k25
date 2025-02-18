4174e6e1c2c8336003ba94cfb3353ddf3e7f6d34
ANA CLARA RIMUALDO PIRES <24122542@CFP127ALUNO.LOCAL>
2025-02-18 09:07:28 -0300

    Criando as consts da CONTROLLER

--- CONTROLLER/musica/controllerMusica.js
+++ CONTROLLER/musica/controllerMusica.js
@@ -1,6 +1,32 @@
-/************************************************************************************************ 
- * Objetivo: Controller referente as ações de CRUD de música
- * Data:     11.02.2025
+/*****************************************************************************************
+ * Objetivo: Controller responsável pela integração entre o APP e a Model (Crud de dados)
+ *  Validações, tratamento de dados, etc...
+ * Data:     18.02.2025
  * Autor:    Ana Pires 
  * Versão:   1.0
-************************************************************************************************/
+*****************************************************************************************/
+
+//função para inserir uma nova música
+const inserirMusica = async function () {
+    
+}
+
+//função para atualizar uma  música
+const atualizarMusica = async function () {
+    
+}
+
+//função para excluir uma  música
+const excluirMusica = async function () {
+    
+}
+
+//função para retornar uma lista de músicas
+const listarMusica = async function () {
+    
+}
+
+//função para retornar uma música pelo ID
+const bucarMusica = async function () {
+    
+}

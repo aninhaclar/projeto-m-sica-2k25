0b9b0dd1648aad5e69144af06a1c5a5eec27bf8d
ANA CLARA RIMUALDO PIRES <24122542@CFP127ALUNO.LOCAL>
2025-02-18 12:24:23 -0300

    App iniciado

--- app.js
+++ app.js
@@ -13,3 +13,53 @@
  *     @prisma/client (npm install @prisma/client --save)
 ************************************************************************************************/
 
+const express = require('express');
+const cors =  require('cors');
+const bodyParser = require('body-parser');
+
+
+
+const app = express()
+
+app.use((request, response, next) => {
+    response.header('Acess-Control-Allow-Origin', '*');
+    response.header('Acess-Control-Allow-MethodsOPTIONS', 'GET, POST, PUT, DELETE, ');
+
+    app.use(cors())
+
+    next()
+})
+
+app.post('/v1/controle-musicas/musica', cors(), async (req, res) => {
+    const musica = req.body;
+    const result = await musicaController.inserirMusica(musica);
+    res.status(result.status_code).json(result);
+});
+
+// Rota para atualizar uma música existente
+app.put('/v1/musicas/:id', async (req, res) => {
+    const id = req.params.id;
+    const musica = req.body;
+    const result = await musicaController.atualizarMusica(id, musica);
+    res.status(result.status_code).json(result);
+});
+
+// Rota para excluir uma música
+app.delete('/v1/musicas/:id', async (req, res) => {
+    const id = req.params.id;
+    const result = await musicaController.excluirMusica(id);
+    res.status(result.status_code).json(result);
+});
+
+// Rota para listar todas as músicas
+app.get('/v1/musicas', async (req, res) => {
+    const result = await musicaController.listarMusica();
+    res.json(result);
+});
+
+// Rota para buscar uma música pelo ID
+app.get('/v1/musicas/:id', async (req, res) => {
+    const id = req.params.id;
+    const result = await musicaController.buscarMusica(id);
+    res.status(result.status_code).json(result);
+});

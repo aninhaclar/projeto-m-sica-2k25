e5f35a330001452e9b410b84b4bd3238dfbaba50
ANA CLARA RIMUALDO PIRES <24122542@CFP127ALUNO.LOCAL>
2025-02-18 09:44:19 -0300

    Criando os if e determinando as características dos atributos

--- CONTROLLER/musica/controllerMusica.js
+++ CONTROLLER/musica/controllerMusica.js
@@ -9,6 +9,17 @@
 //função para inserir uma nova música
 const inserirMusica = async function () {
     
+    if(   musica.nome            == ''    ||  musica.nome            == null || musica.nome            == undefined || musica.nome.lenght            > 100        ||
+          musica.duracao         == ''    ||  musica.duracao         == null || musica.duracao         == undefined || musica.duracao.lenght         > 5          ||
+          musica.data_lancamento == ''    ||  musica.data_lancamento == null || musica.data_lancamento == undefined || musica.data_lancamento.lenght > 10         ||
+                                                                                musica.letra           == undefined ||
+                                                                                musica.link            == undefined || musica.link.lenght            > 200 
+
+    )
+    {
+        return {}
+    }
+
 }
 
 //função para atualizar uma  música
